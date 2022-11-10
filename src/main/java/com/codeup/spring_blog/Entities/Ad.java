00a8339f22d4888ba882188d9895647f8c7a400d
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-11-10 11:36:30 -0600

    learning entities with jpa

---
+++ src/main/java/com/codeup/spring_blog/Entities/Ad.java
@@ -0,0 +1,25 @@
+package com.codeup.spring_blog.Entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ads")
+public class Ad{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+}
+/* THE ABOVE ENTITY EQUALS
+* CREATE TABLE ads (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+* */
